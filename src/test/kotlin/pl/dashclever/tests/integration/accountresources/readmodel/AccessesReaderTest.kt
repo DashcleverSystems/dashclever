acb29652852b88a7ef73adf77fb215c3e53f8267
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-09-12 17:48:06 +0200

    Add tests for AccessesReader

---
+++ src/test/kotlin/pl/dashclever/tests/integration/accountresources/readmodel/AccessesReaderTest.kt
@@ -0,0 +1,47 @@
+package pl.dashclever.tests.integration.accountresources.readmodel
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import pl.dashclever.accountresources.account.model.Account
+import pl.dashclever.accountresources.account.model.AccountRepository
+import pl.dashclever.accountresources.account.readmodel.AccessesReader
+import pl.dashclever.tests.integration.TestcontainersInitializer
+import pl.dashclever.tests.integration.accountresources.AccountCleaner
+
+@SpringBootTest
+@ContextConfiguration(initializers = [TestcontainersInitializer::class])
+internal class AccessesReaderTest(
+    @Autowired private val accountRepository: AccountRepository,
+    @Autowired private val accessesReader: AccessesReader,
+    @Autowired private val accountCleaner: AccountCleaner,
+) {
+
+    @AfterEach
+    fun deleteAccounts() {
+        accountCleaner.deleteAll()
+    }
+
+    @Test
+    fun `should return owner accesses`() {
+        // given
+        val account = Account(
+            username = "username",
+            passwordHash = "passwordHash",
+            email = "email@email.com"
+        )
+        account.createWorkshop("workshopName")
+        accountRepository.save(account)
+
+        // when
+        val result = accessesReader.findWorkshopOwnerAccesses(account.id)
+
+        // then
+        assertThat(result).singleElement().satisfies({
+            assertThat(it.workshopName).isEqualTo("workshopName")
+        })
+    }
+}
