3561200bddb9fab8185907c0f8234e77a3b6407a
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-09-12 17:56:51 +0200

    Add Account and Credentials readers

--- src/main/kotlin/pl/dashclever/spring/security/SecurityConfig.kt
+++ src/main/kotlin/pl/dashclever/spring/security/SecurityConfig.kt
@@ -10,18 +10,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import pl.dashclever.accountresources.account.readmodel.AccountReader
-
+import pl.dashclever.accountresources.account.readmodel.CredentialsReader
 
 @Configuration
 internal class SecurityConfig(
-    private val accountReader: AccountReader,
+    private val credentialsReader: CredentialsReader,
     private val corsFilter: CorsFilter
 ) {
 
     @Bean
     fun userDetailsService(): UserDetailsService =
-        EntryUserDetailsService(accountReader)
+        EntryUserDetailsService(credentialsReader)
 
     @Bean
     fun passwordEncoder(): PasswordEncoder =
