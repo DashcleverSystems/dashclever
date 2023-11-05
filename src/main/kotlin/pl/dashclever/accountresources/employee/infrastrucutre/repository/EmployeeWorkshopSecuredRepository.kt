428e48045f0e9c5dad9c0a4de0eb6cff8b124492
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-11-05 09:53:59 +0100

    Implement interface of employee repository

--- src/main/kotlin/pl/dashclever/accountresources/employee/infrastrucutre/repository/EmployeeWorkshopSecuredRepository.kt
+++ src/main/kotlin/pl/dashclever/accountresources/employee/infrastrucutre/repository/EmployeeWorkshopSecuredRepository.kt
@@ -1,21 +1,37 @@
 package pl.dashclever.accountresources.employee.infrastrucutre.repository
 
+import jakarta.persistence.EntityManager
+import org.springframework.stereotype.Repository
 import pl.dashclever.accountresources.employee.Employee
 import pl.dashclever.accountresources.employee.EmployeeRepository
+import pl.dashclever.commons.security.CurrentAccessProvider
+import pl.dashclever.commons.security.EntitySecurityRecordRepository
 import java.util.*
 
-class EmployeeWorkshopSecuredRepository : EmployeeRepository {
+@Repository
+class EmployeeWorkshopSecuredRepository(
+    private val entityManager: EntityManager,
+    private val securityRecordRepository: EntitySecurityRecordRepository<Employee, UUID, WorkshopEmployee>,
+    private val currentAccessProvider: CurrentAccessProvider,
+    private val employeeWorkshopSecuredJpaRepository: EmployeeWorkshopSecuredJpaRepository
+) : EmployeeRepository {
 
     override fun save(employee: Employee): Employee {
-        TODO("Not yet implemented")
+        entityManager.persist(employee)
+        if (securityRecordRepository.doesSecurityRecordExistFor(employee))
+            return employee
+        val currentAccessWorkshop = currentAccessProvider.currentWorkshop()
+        securityRecordRepository.create(WorkshopEmployee(currentAccessWorkshop.workshopId, employee.id))
+        return employee
     }
 
     override fun findById(id: UUID): Optional<Employee> {
-        TODO("Not yet implemented")
+        val currentWorkshopAccess = currentAccessProvider.currentWorkshop()
+        return employeeWorkshopSecuredJpaRepository.findById(currentWorkshopAccess.workshopId, id)
     }
 
     override fun findByWorkshopId(workshopId: UUID): Set<Employee> {
-        TODO("Not yet implemented")
+        val currentWorkshopAccess = currentAccessProvider.currentWorkshop()
+        return employeeWorkshopSecuredJpaRepository.findByWorkshopId(currentWorkshopAccess.workshopId)
     }
-
 }
