d6e46f3c450539343c1e64be6ede732d9cc50864
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-11-29 17:17:14 +0100

    #56 Set up UI controls

--- web/src/app/shared/commons/commons.module.ts
+++ web/src/app/shared/commons/commons.module.ts
@@ -1,19 +1,21 @@
-import {NgModule} from '@angular/core';
-import {LanguageSelector} from './language-selector/language-selector.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {CommonModule} from '@angular/common';
-import {PrimeModule} from '../prime.module';
-import {AccessesModule} from './accesses-selector/accesses-selector.module';
-import {AccessesSelectorComponent} from './accesses-selector/accesses-selector.component';
-import {ManageStaffComponent} from './manage-staff/manage-staff.component';
-import {AppCardComponent} from './card/card.component';
-import {EstimateCatalogueComponent} from './estimate-catalogue/estimate-catalogue.component';
-import {EstimateFormComponent} from './estimate-catalogue/estimate-form/estimate-form.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AppSpinner} from './spinner/spinner.component';
-import {ManageStaffModule} from '@shared/commons/manage-staff/manage-staff.module';
-import {EstimatePageComponent} from "@shared/commons/estimate-catalogue/estimate-page/estimate-page.component";
-import {PaginatorModule} from "primeng/paginator";
+import { NgModule } from '@angular/core';
+import { LanguageSelector } from './language-selector/language-selector.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { PrimeModule } from '../prime.module';
+import { AccessesModule } from './accesses-selector/accesses-selector.module';
+import { AccessesSelectorComponent } from './accesses-selector/accesses-selector.component';
+import { ManageStaffComponent } from './manage-staff/manage-staff.component';
+import { AppCardComponent } from './card/card.component';
+import { EstimateCatalogueComponent } from './estimate-catalogue/estimate-catalogue.component';
+import { EstimateFormComponent } from './estimate-catalogue/estimate-form/estimate-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppSpinner } from './spinner/spinner.component';
+import { ManageStaffModule } from '@shared/commons/manage-staff/manage-staff.module';
+import { EstimatePageComponent } from '@shared/commons/estimate-catalogue/estimate-page/estimate-page.component';
+import { PaginatorModule } from 'primeng/paginator';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -33,6 +35,8 @@ import {PaginatorModule} from "primeng/paginator";
     ManageStaffModule,
     AppCardComponent,
     AppSpinner,
+    InputTextModule,
+    CalendarModule,
   ],
   exports: [
     LanguageSelector,
@@ -44,5 +48,4 @@ import {PaginatorModule} from "primeng/paginator";
   ],
   providers: [],
 })
-export class CommonsModule {
-}
+export class CommonsModule {}
