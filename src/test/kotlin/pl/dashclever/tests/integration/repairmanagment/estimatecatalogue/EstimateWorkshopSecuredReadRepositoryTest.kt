8f5904c6f9bd89a81418f2b80f90f51a3eee7489
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-11-30 18:19:00 +0100

    #56 Write tests for filtering estimates by creationTimestamp

--- src/test/kotlin/pl/dashclever/tests/integration/repairmanagment/estimatecatalogue/EstimateWorkshopSecuredReadRepositoryTest.kt
+++ src/test/kotlin/pl/dashclever/tests/integration/repairmanagment/estimatecatalogue/EstimateWorkshopSecuredReadRepositoryTest.kt
@@ -3,26 +3,34 @@ package pl.dashclever.tests.integration.repairmanagment.estimatecatalogue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.data.auditing.AuditingHandler
 import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import pl.dashclever.commons.security.Access.WithAuthorities.Authority
 import pl.dashclever.repairmanagment.estimatecatalogue.EstimateSpecifications
 import pl.dashclever.repairmanagment.estimatecatalogue.infrastrucutre.repository.EstimateWorkshopSecuredRepository
+import pl.dashclever.repairmanagment.estimatecatalogue.infrastrucutre.repository.WorkshopEstimate
 import pl.dashclever.tests.integration.TestcontainersInitializer
 import pl.dashclever.tests.integration.repairmanagment.`new estimate`
 import pl.dashclever.tests.integration.spring.TestAccess
 import pl.dashclever.tests.integration.spring.TestAccessSetter
-import java.util.UUID
+import java.time.LocalDateTime
+import java.util.*
 
 @SpringBootTest
+@ExtendWith(MockitoExtension::class)
 @Transactional
 @ContextConfiguration(initializers = [TestcontainersInitializer::class])
 internal class EstimateWorkshopSecuredReadRepositoryTest(
     @Autowired private val testee: EstimateWorkshopSecuredRepository,
-    @Autowired private val estimateWorkshopSecurityRecordTestReadRepository: EstimateWorkshopSecurityRecordTestReadRepository
+    @Autowired private val estimateWorkshopSecurityRecordTestReadRepository: EstimateWorkshopSecurityRecordTestReadRepository,
+    @Autowired @SpyBean private val auditingHandler: AuditingHandler
 ) {
 
     private val testAccessSetter = TestAccessSetter()
@@ -30,6 +38,7 @@ internal class EstimateWorkshopSecuredReadRepositoryTest(
     @Test
     fun `should create estimate and record informing to which workshop it belongs`() {
         // given
+
         val access = TestAccess(
             accountId = UUID.randomUUID(),
             authorities = Authority.values().toSet(),
@@ -40,6 +49,10 @@ internal class EstimateWorkshopSecuredReadRepositoryTest(
 
         // when
         testee.save(estimate)
+
+        // then
+        val estimateSecurityRecords: Set<WorkshopEstimate> = estimateWorkshopSecurityRecordTestReadRepository.findAllByWorkshopId(access.workshopId)
+        assertThat(estimateSecurityRecords).anyMatch { it.estimateId == estimate.id && it.workshopId == access.workshopId }
     }
 
     @Test
@@ -104,6 +117,46 @@ internal class EstimateWorkshopSecuredReadRepositoryTest(
             assertThat(anotherWorkshopEstimateIds).contains(estimate.id)
             assertThat(estimate.estimateId).isEqualTo("25/2023dk")
         }
+    }
+
+    @Test
+    fun `should find only one estimate created after given time`() {
+        // given
+        val friday = LocalDateTime.of(2023, 12, 1, 11, 0)
+        val thursday = friday.minusDays(1)
+        val wednesday = thursday.minusDays(1)
+
+        auditingHandler.setDateTimeProvider { Optional.of(wednesday) }
+
+        val access = TestAccess(
+            accountId = UUID.randomUUID(),
+            authorities = Authority.values().toSet(),
+            workshopId = UUID.randomUUID()
+        )
+        testAccessSetter.setAccess(access)
+        testee.save(`new estimate`("24/2023dk"))
+
+        auditingHandler.setDateTimeProvider { Optional.of(thursday) }
+
+        val anotherAccess = TestAccess(
+            accountId = UUID.randomUUID(),
+            authorities = Authority.values().toSet(),
+            workshopId = UUID.randomUUID()
+        )
+        testAccessSetter.setAccess(anotherAccess)
+        testee.save(`new estimate`("25/2023dk"))
+
+        // when
+        auditingHandler.setDateTimeProvider { Optional.of(friday) }
+        val result = testee.findAll(
+            EstimateSpecifications.createdOnAfter(thursday.minusHours(2)),
+            PageRequest.of(0, 10)
+        )
+
+        // then
+        assertThat(result).singleElement().satisfies({ estimate ->
+            assertThat(estimate.estimateId).isEqualTo("25/2023dk")
+        })
     }
 
     @Test
