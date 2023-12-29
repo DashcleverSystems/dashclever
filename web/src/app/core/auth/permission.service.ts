f8af0161cf28414644c28727620258fe95b3ece0
damiankaplonspa <damian.kaplon@sportalliance.com>
2023-12-29 11:51:45 +0100

    #85 Change permission service to use only api as source of information if user is logged in

--- web/src/app/core/auth/permission.service.ts
+++ web/src/app/core/auth/permission.service.ts
@@ -5,16 +5,8 @@ import {
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { catchError, map, Observable, of, switchMap, tap } from 'rxjs';
-import {
-  currentPermissions,
-  isAuthorized,
-} from '../store/core-store.selectors';
+import { catchError, map, Observable, of, switchMap } from 'rxjs';
 import { ToastService } from '@app/shared/services/toast.service';
-import { HttpClient } from '@angular/common/http';
-import { coreStoreActions } from '../store/core-store.actions';
-import { IWorkshop } from '@app/shared/models/workshop';
 import { AccessDto } from '@api/models/accessDto';
 import { AccountRestApiService } from '@api/services/accountRestApi.service';
 
@@ -24,10 +16,8 @@ import { AccountRestApiService } from '@api/services/accountRestApi.service';
 export class PermissionService {
   constructor(
     private router: Router,
-    private store: Store,
     private accountApi: AccountRestApiService,
     private toast: ToastService,
-    private http: HttpClient,
   ) {}
 
   canActivate(
@@ -35,7 +25,6 @@ export class PermissionService {
     state: RouterStateSnapshot,
   ): boolean | Observable<boolean> {
     const permissions = next.data['permissions'];
-
     if (Array.isArray(permissions) && permissions.length > 0) {
       return this.checkPermissions(permissions).pipe(
         switchMap((permitted) => (!permitted ? this.notPermitted() : of(true))),
@@ -48,61 +37,21 @@ export class PermissionService {
   }
 
   isAuthorized(): Observable<boolean> {
-    return this.store.select(isAuthorized).pipe(
-      switchMap((isAuth) => {
-        if (!isAuth) {
-          return this.isLogged().pipe(
-            switchMap(() => this.getPermissions()),
-            tap((workshops) => {
-              this.store.dispatch(
-                coreStoreActions.loginSuccessfully({ logged: true }),
-              );
-              this.store.dispatch(
-                coreStoreActions.changeWorkshops({ workshops }),
-              );
-            }),
-            switchMap(() => of(true)),
-            catchError(() => this.notAuthorized()),
-          );
-        }
-
-        return of(isAuth);
-      }),
+    return this.accountApi.currentUser().pipe(
+      map(() => true),
+      catchError(() => of(false)),
     );
-  }
-
-  hasPermission(permission: string): Observable<boolean> {
-    return this.store.select(currentPermissions).pipe(
-      switchMap((avPermissions) => {
-        return of(!!avPermissions.find((p) => p === permission));
-      }),
-    );
-  }
-
-  hasAnyPermission(permissions: string[]): Observable<boolean> {
-    return this.store
-      .select(currentPermissions)
-      .pipe(
-        switchMap((avPermissions) =>
-          of(permissions.some((p) => avPermissions.includes(p))),
-        ),
-      );
-  }
-
-  getPermissions() {
-    return this.http.get<IWorkshop[]>('api/account/access');
   }
 
   private checkPermissions(permissions: string[]): Observable<boolean> {
-    const permissionsFromApi$ = this.accountApi.currentUser().pipe(
-      map((accessDto: AccessDto) => Array.from(accessDto.authorities)),
-      map((authorities) => authorities.map((auth) => auth.toString())),
-    );
+    const permissionsFromApi$: Observable<string[]> = this.accountApi
+      .currentUser()
+      .pipe(map((accessDto: AccessDto) => Array.from(accessDto.authorities)));
     return permissionsFromApi$.pipe(
-      switchMap(([store, api]) => {
+      switchMap((permissionsFromApi: string[]) => {
         return of(
-          permissions.every(
-            (perm) => store.includes(perm) || api.includes(perm),
+          permissions.every((permission: string) =>
+            permissionsFromApi.includes(permission),
           ),
         );
       }),
@@ -121,22 +70,6 @@ export class PermissionService {
     this.router.navigate([currentUrl]);
 
     return of(false);
-  }
-
-  private notAuthorized(): Observable<false> {
-    this.toast.error({
-      title: 'toast.unauthorized.title',
-      message: 'toast.unauthorized.message',
-      translate: true,
-    });
-
-    this.router.navigate(['login']);
-
-    return of(false);
-  }
-
-  private isLogged(): Observable<any> {
-    return this.http.get('/api/account');
   }
 }
 
