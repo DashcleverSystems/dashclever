8e03243d6ef455f886ea8cb856d31d7b4db1c9d9
damiankaplonspa <damian.kaplon@sportalliance.com>
2024-03-25 20:15:59 +0100

    Handle events async

--- src/main/kotlin/pl/dashclever/commons/events/DomainEventsConfig.kt
+++ src/main/kotlin/pl/dashclever/commons/events/DomainEventsConfig.kt
@@ -3,6 +3,10 @@ package pl.dashclever.commons.events
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ApplicationEventMulticaster
+import org.springframework.context.event.SimpleApplicationEventMulticaster
+import org.springframework.core.task.SimpleAsyncTaskExecutor
+
 
 @Configuration
 class DomainEventsConfig {
@@ -10,4 +14,11 @@ class DomainEventsConfig {
     @Bean
     fun domainEvents(applicationEventPublisher: ApplicationEventPublisher): DomainEvents =
         InMemoryDomainEventsPublisher(applicationEventPublisher)
+
+    @Bean(name = ["applicationEventMulticaster"])
+    fun simpleApplicationEventMulticaster(): ApplicationEventMulticaster {
+        val eventMulticaster = SimpleApplicationEventMulticaster()
+        eventMulticaster.setTaskExecutor(SimpleAsyncTaskExecutor())
+        return eventMulticaster
+    }
 }
