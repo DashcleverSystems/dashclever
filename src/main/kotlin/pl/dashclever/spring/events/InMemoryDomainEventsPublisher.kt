236c1974b2d267a6171200f8beaae06093335ad9
damiankaplonspa <141049604+damiankaplonspa@users.noreply.github.com>
2024-04-08 19:32:15 +0200

    Simple in memory domain event publishing (#150)
    Simple in memory domain event publishing mechanism

---
+++ src/main/kotlin/pl/dashclever/spring/events/InMemoryDomainEventsPublisher.kt
@@ -0,0 +1,26 @@
+package pl.dashclever.spring.events
+
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.transaction.support.TransactionSynchronization
+import org.springframework.transaction.support.TransactionSynchronizationManager
+import pl.dashclever.commons.events.DomainEvent
+import pl.dashclever.commons.events.DomainEvents
+
+class InMemoryDomainEventsPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher
+) : DomainEvents {
+
+    override fun publish(event: DomainEvent) {
+        if (TransactionSynchronizationManager.isActualTransactionActive().not()) {
+            applicationEventPublisher.publishEvent(event)
+            return
+        }
+        TransactionSynchronizationManager.registerSynchronization(
+            object : TransactionSynchronization {
+                override fun afterCommit() {
+                    applicationEventPublisher.publishEvent(event)
+                }
+            }
+        )
+    }
+}
