1af422a767e69db9b238e9f783ca29843378c7ea
damiankaplon <kaplon.damian99@gmail.com>
2024-04-21 12:08:47 +0200

    Do not allow to start a repair if there is another repair started of the plan of the same estimate

--- src/main/kotlin/pl/dashclever/repairmanagment/repairing/application/RepairService.kt
+++ src/main/kotlin/pl/dashclever/repairmanagment/repairing/application/RepairService.kt
@@ -2,6 +2,7 @@ package pl.dashclever.repairmanagment.repairing.application
 
 import org.springframework.stereotype.Component
 import pl.dashclever.commons.events.DomainEvents
+import pl.dashclever.commons.exception.DomainException
 import pl.dashclever.repairmanagment.plannig.model.PlanRepository
 import pl.dashclever.repairmanagment.repairing.model.Repair
 import pl.dashclever.repairmanagment.repairing.model.RepairEvent.StartedRepairOfPlan
@@ -16,6 +17,11 @@ class RepairService(
 
     fun createRepairOfPlan(planId: UUID): Repair {
         val plan = planRepository.findByIdOrThrow(planId)
+        val allPlansOfEstimateId = planRepository.findAllByEstimateId(plan.estimateId)
+        val allPlanIdsOfEstimateId = allPlansOfEstimateId.mapTo(mutableSetOf()) { it.id }
+        if (repairRepository.anyRunningRepairOfPlanIdIn(allPlanIdsOfEstimateId)) {
+            throw DomainException("Tried to start another repair of estimate id: ${plan.estimateId} with planId: $planId")
+        }
         val repair = Repair(plan.id)
         repairRepository.save(repair)
         domainEvents.publish(StartedRepairOfPlan(repair.id, plan.id))
--- src/test/kotlin/pl/dashclever/tests/unit/repairmanagment/repairing/RepairStartingTests.kt
+++ src/test/kotlin/pl/dashclever/tests/unit/repairmanagment/repairing/RepairStartingTests.kt
@@ -4,8 +4,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import pl.dashclever.commons.events.DomainEvents
+import pl.dashclever.commons.exception.DomainException
 import pl.dashclever.repairmanagment.plannig.model.Plan
 import pl.dashclever.repairmanagment.plannig.model.PlanRepository
 import pl.dashclever.repairmanagment.repairing.application.RepairRepository
@@ -13,7 +15,7 @@ import pl.dashclever.repairmanagment.repairing.application.RepairService
 import pl.dashclever.repairmanagment.repairing.model.RepairEvent.StartedRepairOfPlan
 import java.util.UUID
 
-class RepairStartingTests {
+internal class RepairStartingTests {
 
     private val repairRepository: RepairRepository = mockk(relaxed = true)
     private val planRepository: PlanRepository = mockk(relaxUnitFun = true)
@@ -21,12 +23,16 @@ class RepairStartingTests {
     private val repairService: RepairService = RepairService(planRepository, repairRepository, domainEvents)
 
     @Test
-    fun `should create repair`() {
+    fun `should create a repair`() {
         // given
         val plan: Plan = mockk()
         val planId = UUID.randomUUID()
         every { plan.id } returns planId
+        val estimateId = UUID.randomUUID()
+        every { plan.estimateId } returns estimateId
         every { planRepository.findByIdOrThrow(planId) } returns plan
+        every { planRepository.findAllByEstimateId(estimateId) } returns setOf(plan)
+        every { repairRepository.anyRunningRepairOfPlanIdIn(setOf(planId)) } returns false
 
         // when
         val repair = repairService.createRepairOfPlan(planId)
@@ -43,12 +49,34 @@ class RepairStartingTests {
         val plan: Plan = mockk()
         val planId = UUID.randomUUID()
         every { plan.id } returns planId
+        val estimateId = UUID.randomUUID()
+        every { plan.estimateId } returns estimateId
         every { planRepository.findByIdOrThrow(planId) } returns plan
+        every { planRepository.findAllByEstimateId(estimateId) } returns setOf(plan)
+        every { repairRepository.anyRunningRepairOfPlanIdIn(setOf(planId)) } returns false
 
         // when
         val repair = repairService.createRepairOfPlan(planId)
 
         // then
         verify { domainEvents.publish(StartedRepairOfPlan(repair.id, planId)) }
+    }
+
+    @Test
+    fun `should not allow to start a repair if there is another repair started of the plan of the same estimate`() {
+        // given
+        val plan: Plan = mockk()
+        val planId = UUID.randomUUID()
+        every { plan.id } returns planId
+        val estimateId = UUID.randomUUID()
+        every { plan.estimateId } returns estimateId
+        every { planRepository.findByIdOrThrow(planId) } returns plan
+        every { planRepository.findAllByEstimateId(estimateId) } returns setOf(plan)
+        every { repairRepository.anyRunningRepairOfPlanIdIn(setOf(planId)) } returns true
+
+        // when & then
+        assertThatThrownBy { repairService.createRepairOfPlan(planId) }
+            .isInstanceOf(DomainException::class.java)
+            .hasMessageContaining("Tried to start another repair of estimate id: ${plan.estimateId} with planId: $planId")
     }
 }
