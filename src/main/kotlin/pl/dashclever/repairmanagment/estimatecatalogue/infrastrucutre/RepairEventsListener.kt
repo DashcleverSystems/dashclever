f7ce836b86057a5dcd1abe165e48184f30bd1c7d
damiankaplon <kaplon.damian99@gmail.com>
2024-04-26 21:05:04 +0200

    Use cloud stream events

--- src/main/kotlin/pl/dashclever/repairmanagment/estimatecatalogue/infrastrucutre/RepairEventsListener.kt
+++ src/main/kotlin/pl/dashclever/repairmanagment/estimatecatalogue/infrastrucutre/RepairEventsListener.kt
@@ -1,18 +1,20 @@
 package pl.dashclever.repairmanagment.estimatecatalogue.infrastrucutre
 
-import org.springframework.context.event.EventListener
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import pl.dashclever.repairmanagment.estimatecatalogue.RepairEventsHandler
 import pl.dashclever.repairmanagment.repairing.model.RepairEvent.StartedRepairOfPlan
 import pl.dashclever.spring.TransactionalRunner
+import pl.dashclever.spring.events.cloudstreams.CloudStreamConsumer
 
-@Component("EstimateCatalogueRepairEventsListener")
+@Configuration("EstimateCatalogueRepairEventsListener")
 internal class RepairEventsListener(
     private val repairEventsHandler: RepairEventsHandler,
-    private val transactionalRunner: TransactionalRunner
+    private val transactionalRunner: TransactionalRunner,
 ) {
 
-    @EventListener
-    fun handle(event: StartedRepairOfPlan) =
+    @Bean
+    fun indicateOngoingRepairOfEstimate() = CloudStreamConsumer<StartedRepairOfPlan> { event ->
         transactionalRunner.run { repairEventsHandler.handle(event) }
+    }
 }
--- src/main/kotlin/pl/dashclever/repairmanagment/plannig/infrastructure/RepairEventsListener.kt
+++ src/main/kotlin/pl/dashclever/repairmanagment/plannig/infrastructure/RepairEventsListener.kt
@@ -1,18 +1,20 @@
 package pl.dashclever.repairmanagment.plannig.infrastructure
 
-import org.springframework.context.event.EventListener
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import pl.dashclever.repairmanagment.plannig.model.RepairEventsHandler
 import pl.dashclever.repairmanagment.repairing.model.RepairEvent.StartedRepairOfPlan
 import pl.dashclever.spring.TransactionalRunner
+import pl.dashclever.spring.events.cloudstreams.CloudStreamConsumer
 
-@Component("PlanningRepairEventsListener")
+@Configuration("PlanningRepairEventsListener")
 internal class RepairEventsListener(
     private val repairEventsHandler: RepairEventsHandler,
     private val transactionalRunner: TransactionalRunner
 ) {
 
-    @EventListener
-    fun handle(event: StartedRepairOfPlan) =
+    @Bean
+    fun blockPlansModifications() = CloudStreamConsumer<StartedRepairOfPlan> { event ->
         transactionalRunner.run { repairEventsHandler.handle(event) }
+    }
 }
