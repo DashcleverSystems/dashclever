648fd885ee36ce5d169d2f7baa744e3fd97e74a2
damiankaplon <kaplon.damian99@gmail.com>
2024-05-29 16:39:56 +0200

    Add cloud stream auth logging

--- src/main/kotlin/pl/dashclever/spring/events/cloudstreams/CloudStreamAuthenticator.kt
+++ src/main/kotlin/pl/dashclever/spring/events/cloudstreams/CloudStreamAuthenticator.kt
@@ -1,5 +1,6 @@
 package pl.dashclever.spring.events.cloudstreams
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Component
 import pl.dashclever.commons.security.SystemAccessSetter
 import pl.dashclever.commons.security.SystemOnBehalfOfWorkshop
 import pl.dashclever.spring.events.cloudstreams.CloudStreamDomainEventsMultitenantProxy.MultitenantDomainEvent
+
+private val logger = KotlinLogging.logger {}
 
 @Aspect
 @Component
@@ -20,8 +23,10 @@ class CloudStreamAuthenticator(
 
     @Before("authenticateCloudStream()")
     fun authenticate(joinPoint: JoinPoint) {
+        logger.info { "Considering cloud stream ${joinPoint.target} to authentication." }
         if (joinPoint.args.any { it is MultitenantDomainEvent<*> }) {
             val event = joinPoint.args.first { it is MultitenantDomainEvent<*> } as MultitenantDomainEvent<*>
+            logger.info { "Authenticating cloud stream ${joinPoint.target} on behalf of workshop ${event.workshopId}." }
             systemAccessSetter.set(SystemOnBehalfOfWorkshop(event.workshopId))
         }
     }
